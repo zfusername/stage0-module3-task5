de8dc10fc6e6843a6bec38aa52948e05ae9ba72d
zfusername <farrukh_ziyaev@epam.com>
2023-07-13 17:43:39 +0500

    change

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        float seconds = minutes * 60;
+        System.out.println(seconds);
     }
 }
