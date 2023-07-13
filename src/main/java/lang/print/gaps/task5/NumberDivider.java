807e12d40340d868e4cfd901105b3fe6da417423
zfusername <farrukh_ziyaev@epam.com>
2023-07-13 17:54:19 +0500

    changed

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
-        float res = toBeDivided / 5;
+        double res = (double)toBeDivided / 5;
         System.out.println(res);
     }
 }
